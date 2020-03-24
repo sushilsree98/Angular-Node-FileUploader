059acb50e9705499a88b5050c35eb1a3b268e849
sushilsree98 <sushil.sree98@gmail.com>
2020-03-24 10:18:45 +0530

    files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fileUpload';
+  userForm: any;
+  form:any;
+  constructor(private http: HttpClient) {
+    this.userForm = new FormGroup({
+      "file": new FormControl('')
+    })
+  }
+
+  fileChange(event) {
+    this.form = event.target.files[0];
+  }
+
+  submitFunc() {
+    const fileData = new FormData();
+    fileData.append('file', this.form);
+    this.http.post("http://localhost:8000/file", fileData)
+      .subscribe(res => {
+        console.log("post successful");
+      })
+  }
 }
