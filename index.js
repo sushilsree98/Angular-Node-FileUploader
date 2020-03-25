17f82bc5e26975c343ff910aa51e526e1b43d0b4
sushil <sushil.sree98@gmail.com>
2020-03-25 10:26:01 +0530

    server file updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const multer=require('multer')
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 
-const storage= multer.diskStorage({
+const storage = multer.diskStorage({
   destination:function(req,file,cb){
     cb(null,'uploads/');
   },
@@ -11,10 +11,32 @@ const storage= multer.diskStorage({
     cb(null, file.originalname);
   }
 });
-const uploads= multer({storage:storage})
+const fileFilter = (req,file,cb)=>{
+    if (file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const uploads = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter
+})
+
+app.use('/uploads',express.static('uploads'));
 app.use(cors());
-app.post('/file',uploads.single('file'),(req,res)=>{
-  console.log(req.file);
+app.post('/file', uploads.single('file'), (req, res) => {
+    res.send(req.file.destination + req.file.originalname);
+
+})
+
+app.get('/file', uploads.single('file'), (req, res) => {
+    res.json(req.body);
+      
 })
 
 app.listen(8000);
